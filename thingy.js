81248e60083af4dcef907231676503171d24992f
Miguel Angel Castillo <321angelcastillo.mc@gmail.com>
2017-05-02 11:26:17 -0700

    loololollolololol f thus

--- thingy.js
+++ thingy.js
@@ -6,7 +6,35 @@ var scoreText = makeText("Score: -", 660, 50, 25, "sans-serif", "white", 1)
 gameOver = false
 
 var player = makeImage("images/pixelcharacter.png", 30, 100, 30, 30, 1)
-var enemy = makeImage("images/enemy.jpg", 30, 50, 30, 30, 1)
+
+/*
+function checkCollisionsMissiles() {
+    for (var j = 0; j < enemies.length; j++) {
+        for (var i = 0; i < missiles.length; i++) {
+          if (collide(missiles[i], enemies[j], 0, -20) == true) {
+                // YOUR CODE HERE
+            }
+        }
+    }
+}*/
+
+function checkCollisionsMissiles() {
+for (var i = 0; i < missiles.length; i++) {
+  for (var j = 0; j < enemies.length; j++) {
+    if (missiles[i] != undefined && enemies[j] != undefined) {
+      if (collide(missiles[i], enemies[j], 0, -20) == true) {
+          drawExplosion(getX(enemies[j]), getY(enemies[j]))
+          removeArrayElement(enemies, j)
+          removeArrayElement(missiles, i)
+          console.log("same")
+          score = score + 1
+          scoreText.innerHTML = "Score: " + score
+                       }
+                   }
+               }
+           }
+       }
+
 
 var enemies = []
 var missiles = []
@@ -15,3 +43,74 @@ function drawPlayer() {
   setX(player, mouse.x)
   setY(player, mouse.y)
 }
+
+function drawEverything() {
+  drawPlayer()
+  drawEnemies()
+  drawMissiles()
+  checkCollisionsPlayer()
+  checkCollisionsMissiles()
+
+  if (gameOver == false) {
+    requestAnimationFrame(drawEverything)
+  }
+}
+
+function makeEnemies() {
+  var enemy = makeImage("images/enemy.jpg", 750, random(0,400), 30, 30, .22)
+  enemies.push(enemy)
+  setTimeout(makeEnemies, 600)
+}
+setTimeout(makeEnemies, 8000)
+
+function drawEnemies() {
+  for (var i = 0; i < enemies.length; i++){
+    move(enemies[i], -4, 0)
+    if (getX(enemies[i]) < 0) {
+      setX(enemies[i], 800)
+    }
+  }
+}
+
+function makeEnemies() {
+  var enemy = makeImage("images/enemy.jpg", 0, random(0,400), 30, 30, .22)
+  enemies.push(enemy)
+  setTimeout(makeEnemies, 600)
+}
+setTimeout(makeEnemies, 8000)
+
+function drawEnemies() {
+  for (var i = 0; i < enemies.length; i++){
+    move(enemies[i], -4, 0)
+    if (getX(enemies[i]) < 0) {
+      setX(enemies[i], 800)
+    }
+  }
+}
+
+function fireMissile() {
+  var rectangle = makeRect(getX(player) + 15, getY(player) + 15, 15, 10, "blue", 1)
+  missiles.push(rectangle)
+}
+
+document.addEventListener("click", fireMissile)
+
+function drawMissiles() {
+  for (var i = 0; i < missiles.length; i++){
+    move(missiles[i], 5, 0)
+  }
+}
+
+function checkCollisionsPlayer() {
+   for(var i = 0; i < enemies.length; i++){
+       if(collide(player, enemies[i]) == true){
+       removeElement(player)
+       removeArrayElement(enemies, i)
+       gameOver = true
+       }
+   }
+}
+
+drawEverything()
+makeEnemies()
+fireMissile()
